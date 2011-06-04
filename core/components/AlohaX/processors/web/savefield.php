83c19a9e828f993cd5fb66f458f6513295a20472
Mark-H <hamstra.mark@gmail.com>
2011-06-04 23:21:26 +0200

    Add some license info

--- core/components/AlohaX/processors/web/savefield.php
+++ core/components/AlohaX/processors/web/savefield.php
@@ -1,5 +1,25 @@
 <?php
-
+/*
+ * AholaX
+ *
+ * Copyright 2011 by Digital Butter <www.butter.com.hk>
+ *
+ * This file is part of AholaX, a port of Ahola Editor (ahola-editor.com) for MODX Revolution.
+ *
+ * AholaX is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * AholaX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * AholaX; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
 $saved = false;
 
 if($resource = $modx->getObject('modResource', $scriptProperties['id'])){
